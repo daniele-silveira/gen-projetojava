a75aa15f9a8a5f58bd18d53e74629f4ab72d7b06
Daniele Napoleao <daniele20@gmail.com>
2025-02-20 11:27:39 -0300

    Variaveis e Lacos

--- src/papelaria/Menu.java
+++ src/papelaria/Menu.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// teste
 
 		Scanner leia = new Scanner(System.in);
 
